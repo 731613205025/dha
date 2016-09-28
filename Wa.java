5466cb52c6eaa3a9416d863af8e4d1c83d1ce2b2
731613205025 <maniharish95@gmail.com>
2016-09-28 13:51:24 +0530

    Add files via upload

---
+++ Wa.java
@@ -0,0 +1,82 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package wa;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Student
+ */
+public class Wa {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+     
+          {
+
+            int n, temp;
+
+            Scanner s = new Scanner(System.in);
+
+            System.out.print("Enter no. of elements you want in array:");
+
+            n = s.nextInt();
+
+            int a[] = new int[n];
+
+            System.out.println("Enter all the elements:");
+
+            for (int i = 0; i < n; i++) 
+
+            {
+
+                a[i] = s.nextInt();
+
+            }
+
+            for (int i = 0; i < n; i++) 
+
+            {
+
+                for (int j = i + 1; j < n; j++) 
+
+                {
+
+                    if (a[i] > a[j]) 
+
+                    {
+
+                        temp = a[i];
+
+                        a[i] = a[j];
+
+                        a[j] = temp;
+
+                    }
+
+                }
+
+            }
+
+            System.out.print("Ascending Order:");
+
+            for (int i = 0; i < n - 1; i++) 
+
+            {
+
+                System.out.print(a[i] + ",");
+
+            }
+
+            System.out.print(a[n - 1]);
+
+        }
+
+    }// TODO code application logic here
+    
+}
